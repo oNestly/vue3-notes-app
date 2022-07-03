f65c63127ca22f9e65da333b8bb301c3f2cb8770
Anthony <onestlyof@gmail.com>
2022-07-03 12:22:59 +0300

    Add: List.vue

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,19 +1,30 @@
 <template>
-	<Form @test="handleTest" @onSubmit="handleSubmit" />
+	<Form @onSubmit="handleSubmit" @test="handleTest"  />
+	<List @onRemove="handleRemove" :items="notes" />
 </template>
 
 <script>
 import Form from "@/components/Note/Form.vue";
+import List from "@/components/Note/List.vue";
 
 export default {
-	components: { Form },
+	components: { Form, List },
+	data() {
+		return {
+			notes: ['Task 1', 'Task 2', 'Task 3']
+		}
+	},
 	methods: {
 		handleSubmit(note) {
+			this.notes.push(note);
 			console.log(note);
 		},
 		handleTest(user) {
 			console.log(user.name);
 			console.log(user.age);
+		},
+		handleRemove(index) {
+			this.notes.splice(index, 1);
 		},
 	}
 };
